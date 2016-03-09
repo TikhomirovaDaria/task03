ad236afb73159b54e0d85871b6b9c16f321d9191
Daria Tikhomirova <tihodrik@gmail.com>
2016-03-10 00:01:58 +0300

    Regular expresions1 was finished

--- Regular expressions1/Program.cs
+++ Regular expressions1/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Regular_expressions
+namespace Regular_expressions1
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Regex loginMask = new Regex(@"^[a-zA-Z][\w]{1,9}$");
+			string login;
+
+			Console.Write("Enter login: ");
+			login = Console.ReadLine();
+
+			Console.WriteLine(loginMask.IsMatch(login));
+			Console.ReadKey();
 		}
 	}
 }
