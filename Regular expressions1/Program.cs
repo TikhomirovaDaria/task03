5f295b8b2644fd630ecbb3a7d0bf8d3577c8866f
Daria Tikhomirova <tihodrik@gmail.com>
2016-03-10 18:58:01 +0300

    in RegExp1: mask was changed, added cycle

--- Regular expressions1/Program.cs
+++ Regular expressions1/Program.cs
@@ -11,14 +11,21 @@ namespace Regular_expressions1
 	{
 		static void Main(string[] args)
 		{
-			Regex loginMask = new Regex(@"^[a-zA-Z][\w]{1,9}$");
+			Regex loginMask = new Regex(@"^[a-z][a-z\d]{1,9}$", RegexOptions.IgnoreCase);
 			string login;
 
-			Console.Write("Enter login: ");
-			login = Console.ReadLine();
+			do
+			{
+				Console.Clear();
+				Console.Write("Enter login: ");
+				login = Console.ReadLine();
 
-			Console.WriteLine(loginMask.IsMatch(login));
-			Console.ReadKey();
+				Console.WriteLine(loginMask.IsMatch(login));
+				Console.Write("\nEnter Esc to exit or any other button to continue ");
+
+			} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+			
 		}
 	}
 }
